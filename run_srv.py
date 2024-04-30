b16b8ad6a672d50c1aa2f8e05b1547f928bb1cce
frdel <38891707+frdel@users.noreply.github.com>
2024-04-30 17:41:29 +0200

    v0

--- run_srv.py
+++ run_srv.py
@@ -33,7 +33,7 @@ async def test_form():
 # simple health check, just return OK to see the server is running
 @app.route('/health', methods=['GET','POST'])
 async def health_check():
-    return "OK version 2"
+    return "OK"
 
 # secret page, requires authentication
 @app.route('/secret', methods=['GET'])
